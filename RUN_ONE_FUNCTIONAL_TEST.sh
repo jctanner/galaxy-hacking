7ec88d2d42394eaef604d2e8caac68007bd14e80
James Tanner <tanner.jc@gmail.com>
2022-06-29 18:53:06 -0400

    more wrapper scripts for ghacktion

---
+++ RUN_ONE_FUNCTIONAL_TEST.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+WORKDIR=/tmp/$(ls -t /tmp | fgrep ghacktion | head -n1)
+PULP_ANSIBLE_DIR=$WORKDIR/pulp/pulp_ansible
+cd $PULP_ANSIBLE_DIR
+source .venv/bin/activate
+
+export PYTHONPATH=$PULP_ANSIBLE_DIR:$PULP_ANSIBLE_DIR/../galaxy-importer 
+
+.venv/bin/pytest \
+    --capture=no \
+    -v \
+    -r sx \
+    --color=yes \
+    --pyargs pulp_ansible.tests.functional \
+    -m "not parallel" \
+    -k test_install_role
---
+++ RUN_PULP_ANSIBLE_EXAMPLE.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /vagrant
+./scripts/docker-killall
+./scripts/docker-rmall
+rm -rf ~/.config/pulp
+sudo rm -rf /tmp/ghacktion*
+
+
+#./ghacktion --local --repo=pulp/pulp_ansible --number=1062 run --file=ci.yml --job=test --noclean
+./ghacktion --local --repo=pulp/pulp_ansible --number=1067 run --file=ci.yml --job=test --noclean
