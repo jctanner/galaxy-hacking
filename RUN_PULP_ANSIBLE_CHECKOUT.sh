301f1e22ca7ea99bcfa2a8e799a2f9f84a8f7f68
James Tanner <tanner.jc@gmail.com>
2022-06-30 10:20:28 -0400

    add another script

---
+++ RUN_PULP_ANSIBLE_CHECKOUT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /vagrant
+./scripts/docker-killall
+./scripts/docker-rmall
+rm -rf ~/.config/pulp
+sudo rm -rf /tmp/ghacktion*
+rm -f ~/.netrc
+
+
+#./ghacktion --local --repo=pulp/pulp_ansible --number=1062 run --file=ci.yml --job=test --noclean
+#./ghacktion --local --repo=pulp/pulp_ansible --number=1067 run --file=ci.yml --job=test --noclean
+./ghacktion --local --checkout=$(pwd)/test_checkouts/pulp_ansible run --file=ci.yml --job=test --noclean
