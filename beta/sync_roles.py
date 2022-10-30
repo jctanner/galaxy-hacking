6b94783a2f8aeb2312056905c10a39d5c607ba51
James Tanner <tanner.jc@gmail.com>
2022-10-30 16:43:20 -0400

    add sync roles script

---
+++ beta/sync_roles.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def main():
+
+    token = os.environ.get('HUB_TOKEN')
+    username = os.environ.get('HUB_USERNAME')
+    password = os.environ.get('HUB_PASSWORD')
+    upstream_baseurl = os.environ.get(
+        'GALAXY_UPSTREAM_BASEURL',
+        'https://galaxy.ansible.com/api/v1/roles/'
+    )
+    downstream_baseurl = os.environ.get(
+        'GALAXY_DOWNSTREAM_BASEURL',
+        'https://beta-galaxy-dev.ansible.com'
+    )
+    sync_url = downstream_baseurl + '/api/v1/sync'
+
+    kwargs = {
+        'json': {'baseurl': baseurl},
+    }
+    if token:
+        headers = {'Authorization': f'token {token}')
+        kwargs['headers'] = headers
+    elif username and password:
+        kwargs['auth'] = (username, password)
+
+    rr = requests.post(sync_url, **kwargs)
+    ds = rr.json()
+
+
+if __name__ == "__main__":
+    main()
